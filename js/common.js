2fd24754dababcf8950fb50097e044e365d8fcc3
angbaebultti <hjh99224@gmail.com>
2026-03-25 16:46:55 +0900

    top

--- js/common.js
+++ js/common.js
@@ -5,6 +5,8 @@
 // 주석으로 어떤 트리거인지 적어주기 ex) 햄버거 메뉴, 스와이퍼 등
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    //햄버거 메뉴
     const menuBtn = document.getElementById("menu_btn");
     const closeBtn = document.getElementById("close_btn");
     const menu = document.getElementById("menu");
@@ -15,6 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     closeBtn.addEventListener("click", () => {
         menu.classList.remove("active");
+    });
+
+
+    // top 버튼 부드럽게 
+    const top_btn = document.querySelector('.top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            top_btn.classList.add('active');
+        } else {
+            top_btn.classList.remove('active');
+        }
     });
 });
 //dom end
